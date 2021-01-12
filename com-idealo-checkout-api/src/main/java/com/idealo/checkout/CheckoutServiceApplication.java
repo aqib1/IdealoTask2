5e21176e3e98e509424e1a614f9df9edf79ebace
DESKTOP-U7ONH0K\Aqib <aqibbutt3078@gmail.com>
2021-01-13 04:12:51 +0500

    Idealo project init setup

---
+++ com-idealo-checkout-api/src/main/java/com/idealo/checkout/CheckoutServiceApplication.java
@@ -0,0 +1,19 @@
+package com.idealo.checkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * <p>
+ * This is the main class for starting doctor portal service
+ * </p>
+ * @author Aqib
+ * @since 01/13/2021
+ * @version 0.0.1
+ * */
+@SpringBootApplication
+public class CheckoutServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CheckoutServiceApplication.class, args);
+    }
+}
