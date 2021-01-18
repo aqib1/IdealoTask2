ba3a0391e796a33f14a31f554796bf6ebe486ff3
DESKTOP-U7ONH0K\Aqib <aqibbutt3078@gmail.com>
2021-01-19 00:07:20 +0500

    Integeration test for check out api

--- com-idealo-checkout-api/src/test/java/com/idealo/checkout/integeration/API/CheckoutApiIT.java
+++ com-idealo-checkout-api/src/test/java/com/idealo/checkout/integeration/API/CheckoutApiIT.java
@@ -1,4 +1,86 @@
 package com.idealo.checkout.integeration.API;
 
+
+import com.idealo.checkout.business.CheckoutBusiness;
+import com.idealo.checkout.clients.ProductClient;
+import com.idealo.checkout.clients.RuleInfoClient;
+import com.idealo.checkout.controller.CheckoutController;
+import com.idealo.checkout.mappers.RuleInfoMapper;
+import com.idealo.checkout.model.CheckoutRequest;
+import com.idealo.checkout.model.RuleRequest;
+import com.idealo.checkout.service.Impl.CheckoutServiceImpl;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.Spy;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static com.idealo.checkout.integeration.helper.DataHelperIT.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@RunWith(SpringRunner.class)
 public class CheckoutApiIT {
+
+    private static final String CHECKOUT_CHECK = "/checkout/check";
+
+    private MockMvc mockMvc;
+
+    @Mock
+    private ProductClient productClient;
+
+    @Mock
+    private RuleInfoClient ruleInfoClient;
+
+    @Spy
+    private RuleInfoMapper mapper;
+
+    @Spy
+    @InjectMocks
+    private CheckoutServiceImpl service;
+
+    @Spy
+    @InjectMocks
+    private CheckoutBusiness business;
+
+    @Spy
+    @InjectMocks
+    private CheckoutController controller;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        this.mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+    }
+
+    @Test
+    public void testCheckout() throws Exception {
+        when(productClient.getAllBySku(any(CheckoutRequest.class)))
+                .thenReturn(getProductBySkuResponse());
+
+        when(ruleInfoClient.pricingRules(any(RuleRequest.class)))
+                .thenReturn(ruleResponse());
+
+        this.mockMvc.perform(post(CHECKOUT_CHECK)
+                .content(asJsonString(checkoutRequest()))
+                .contentType(MEDIA_TYPE_JSON_UTF8))
+                .andDo(print())
+                .andExpect(content().contentType(MEDIA_TYPE_JSON_UTF8))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.total").value(300))
+                .andExpect(jsonPath("$.ruleInfoResponse[0].sku").value("SKU"))
+                .andExpect(jsonPath("$.ruleInfoResponse[0].checkoutQuantity").value(6))
+                .andExpect(jsonPath("$.ruleInfoResponse[0].shipping").value(5))
+                .andExpect(jsonPath("$.ruleInfoResponse[0].specialPrice").value(300))
+                .andExpect(jsonPath("$.ruleInfoResponse[0].detailedMessage").value("Free shipping for buying 5 items"));
+
+    }
 }
---
+++ com-idealo-checkout-api/src/test/java/com/idealo/checkout/integeration/helper/DataHelperIT.java
@@ -0,0 +1,73 @@
+package com.idealo.checkout.integeration.helper;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.collect.Lists;
+import com.idealo.checkout.model.*;
+import org.springframework.http.MediaType;
+
+public class DataHelperIT {
+
+    public static MediaType MEDIA_TYPE_JSON_UTF8 = new MediaType("application", "json");
+
+    public static CheckoutRequest checkoutRequest() {
+        return new CheckoutRequest().checkoutInfo(Lists.newArrayList(new CheckoutInfo().checkoutQuantity(6l).sku("SKU")));
+    }
+
+    public static GetProductBySkuResponse getProductBySkuResponse() {
+        return new GetProductBySkuResponse().productShortResponseList(
+                Lists.newArrayList(
+                        new ProductShortResponse()
+                                .productId("Test Prod 1")
+                                .sku("SKU")
+                                .shipping(5l)
+                                .unitPrice(50l)
+                                .quantity(10l)
+                                .detailedMessage(""))
+        );
+    }
+
+    public static RuleResponse ruleResponse() {
+        return new RuleResponse().total(300l).ruleInfoResponse(
+                Lists.newArrayList(
+                        new RuleInfoResponse()
+                                .shipping(5l)
+                                .specialPrice(300l)
+                                .sku("SKU")
+                                .checkoutQuantity(6l)
+                                .unitPrice(50l)
+                                .detailedMessage("Free shipping for buying 5 items")
+                )
+        );
+    }
+
+    public static String asJsonString(final Object obj) {
+        try {
+            return new ObjectMapper().writeValueAsString(obj);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    /**
+     * Initialization on demand pattern
+     * this pattern is alternative of double check locking pattern
+     * which not even support lazy loading but also safe to use in
+     * multi-processor distributed instances
+     */
+    private static class InstanceHolder {
+        private static final DataHelperIT INSTANCE = new DataHelperIT();
+
+        private InstanceHolder() {
+
+        }
+    }
+
+
+    public static DataHelperIT getInstance() {
+        return DataHelperIT.InstanceHolder.INSTANCE;
+    }
+
+    private DataHelperIT() {
+
+    }
+}
